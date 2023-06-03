0fab79c7bfb51d5349e23cf54138a985b95a3e38
Mcjerry <mcjerry73@gmail.com>
2023-06-03 19:54:39 +0100

    fds

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -124,13 +124,14 @@ const Video = () => {
   const path = useLocation().pathname.split("/")[2];
 
   const [channel, setChannel] = useState({});
+  const baseUrl = "https://mtube-api.onrender.com/api";
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const videoRes = await axios.get(`/videos/find/${path}`);
+        const videoRes = await axios.get(`${baseUrl}/videos/find/${path}`);
         const channelRes = await axios.get(
-          `/users/find/${videoRes.data.userId}`
+          `${baseUrl}/users/find/${videoRes.data.userId}`
         );
         setChannel(channelRes.data);
         dispatch(fetchSuccess(videoRes.data));
@@ -140,18 +141,18 @@ const Video = () => {
   }, [path, dispatch]);
 
   const handleLike = async () => {
-    await axios.put(`/users/like/${currentVideo._id}`);
+    await axios.put(`${baseUrl}/users/like/${currentVideo._id}`);
     dispatch(like(currentUser._id));
   };
   const handleDislike = async () => {
-    await axios.put(`/users/dislike/${currentVideo._id}`);
+    await axios.put(`${baseUrl}/users/dislike/${currentVideo._id}`);
     dispatch(dislike(currentUser._id));
   };
 
   const handleSub = async () => {
     currentUser.subscribedUsers.includes(channel._id)
-      ? await axios.put(`/users/unsub/${channel._id}`)
-      : await axios.put(`/users/sub/${channel._id}`);
+      ? await axios.put(`${baseUrl}/users/unsub/${channel._id}`)
+      : await axios.put(`${baseUrl}/users/sub/${channel._id}`);
     dispatch(subscription(channel._id));
   };
 
